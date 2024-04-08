af5c061019665f32b2aa9f06d9625546df4cad46
BrunoMano2004 <brunomano2004@gmail.com>
2024-04-08 20:40:56 -0300

    Adicionando método e view para listar motoristas

--- aula5/cadastroMotorista/src/main/java/cadastroMotorista/cadastroMotorista/controller/MotoristaController.java
+++ aula5/cadastroMotorista/src/main/java/cadastroMotorista/cadastroMotorista/controller/MotoristaController.java
@@ -1,9 +1,12 @@
 package cadastroMotorista.cadastroMotorista.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import cadastroMotorista.cadastroMotorista.model.MotoristaTO;
 import cadastroMotorista.cadastroMotorista.repository.MotoristaRepository;
@@ -27,5 +30,14 @@ public class MotoristaController {
 		motoristaRepository.save(obj);
 		
 		return "cadastroMotorista";
+	}
+	
+	@GetMapping("/listarMotoristas")
+	public ModelAndView listarJogadores() {
+		ModelAndView motoristaMV = new ModelAndView("listarMotoristas");
+		List<MotoristaTO> listaMotoristas = (List<MotoristaTO>)motoristaRepository.findAll();
+		motoristaMV.addObject("motoristas", listaMotoristas);
+		
+		return motoristaMV;
 	}
 }
